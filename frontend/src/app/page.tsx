e30159e1f11bcfce94184f147d5852806f986cc2
Gabriel Mikio <gmikioas@gmail.com>
2024-09-18 16:43:08 -0300

    Update: Translated texts from English to Portuguese

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,16 +3,15 @@ import Link from 'next/link';
 export default function Home(){
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center">
-            <h1 className="text-5xl font-black text-black mb-8">Welcome to Patronos App</h1>
-            <p className="text-xl text-black mb-8">Please choose an option below:</p>
-
+            <h1 className="text-4xl font-black text-black mb-8">Patronos</h1>
+            <h1 className="text-5xl font-black text-black mb-12">Centro de Carreiras</h1>
             {/* Links to other pages */}
             <div className="space-x-4">
                 <Link className="bg-blue-600 text-white py-3 px-6 rounded-lg shadow-md hover:bg-blue-700 transition duration-300" href="/login">
-                    Go to Login
+                    Login
                 </Link>
                 <Link className='bg-green-600 text-white py-3 px-6 rounded-lg shadow-md hover:bg-green-700 transition duration-300' href="/mentoria">
-                    Go to Mentoria
+                    Mentoria
                 </Link>
             </div>
         </div>
