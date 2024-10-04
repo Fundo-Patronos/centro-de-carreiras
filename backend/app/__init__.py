e80aae22005f727493b57729e2824f9a269f4894
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2024-10-03 22:32:15 -0300

    feat: add cors to allow frontend requests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,25 @@
+import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
+front_end_url = os.getenv("FRONT_END_BASE_URL", None)
+
+if front_end_url is None:
+    raise ValueError("FRONT_END_BASE_URL is not set")
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[front_end_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth_router)
