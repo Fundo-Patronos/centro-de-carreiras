7f4f914c631d3e52f9f356c1c268c659ddd97ba5
Bruno Freitas <brunoatf257@gmail.com>
2024-10-04 12:42:33 -0300

    added functioning scheduling page

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import mentoring
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permite seu front-end em localhost:3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 app.include_router(mentoring.router)
