cec68c1acb3e0cebc28103af99ed2f9f80ec6089
Andreas Cisi Ramos <deascisi@hotmail.com>
2024-10-04 11:18:40 -0300

    Improving verify page

--- frontend/src/app/verify/[token]/page.tsx
+++ frontend/src/app/verify/[token]/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useParams } from 'next/navigation'; // Importe useParams
+import { useRouter, useParams } from 'next/navigation';
 import Button from '../../../components/GradientButton';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const VerifyEmail = () => {
-    const { token } = useParams(); // Use useParams para obter o token
+    const { token } = useParams(); 
     const router = useRouter();
     const [message, setMessage] = useState('Verificando seu e-mail...');
     const [loading, setLoading] = useState(true);
@@ -40,8 +39,7 @@ const VerifyEmail = () => {
     };
 
     useEffect(() => {
-        // Verifique se o token existe
-        if (token) {
+        if (token && typeof token === 'string') {
             console.log("Token encontrado:", token);
             verifyEmail(token);
         } else {
@@ -49,27 +47,35 @@ const VerifyEmail = () => {
             setMessage("Token não fornecido.");
             setLoading(false);
         }
-    }, [token]); // Apenas o token como dependência
+    }, [token]); 
 
     const handleLoginRedirect = () => {
-        router.push("/"); // Redireciona para a página de login
+        router.push("/"); 
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen text-center">
-            <h1 className={`text-2xl font-bold ${loading ? "text-gray-500" : "text-green-600"}`}>
-                {loading ? "Verificando seu e-mail. Aguarde, por favor..." : message}
-            </h1>
-            {!loading && (
-                <Button
-                    onClick={handleLoginRedirect}
-                    className="mt-5 px-4 py-2"
-                >
-                    Login
-                </Button>
-            )}
+        <div
+            className="flex flex-col items-center justify-center h-screen bg-[rgb(255,255,255,0.9)] text-center px-4"
+            // style={{ backgroundImage: 'url("/images/verify-background.png")' }} 
+        >
+            <div className="bg-white p-6 rounded-lg shadow-2xl w-full max-w-md"> 
+                <h1 className="text-2xl font-bold text-gray-700 mb-4">
+                    {loading ? "Verificando seu e-mail. Aguarde, por favor..." : message}
+                </h1>
+                {!loading && (
+                    <Button
+                        onClick={handleLoginRedirect}
+                        className="mt-5 w-full sm:w-auto px-6 py-2"
+                    >
+                        Login
+                    </Button>
+                )}
+            </div>
         </div>
     );
+    
+    
+    
 };
 
 export default VerifyEmail;
