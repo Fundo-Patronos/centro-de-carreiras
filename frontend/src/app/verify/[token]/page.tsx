f1edb2501a4f7a656cdb89363c7caccf5acf8f37
Andreas Cisi Ramos <82724293+TheDeas343@users.noreply.github.com>
2024-10-25 11:25:03 -0300

    Hero Page [PR: feature/hero-page -> staging] (#84)
    * HeroPage - Creating Hero Login Page
    * HeroPage - Improving the Responsiveness
    * VerifyPage - Animated Screen

--- frontend/src/app/verify/[token]/page.tsx
+++ frontend/src/app/verify/[token]/page.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 import Button from "../../../components/GradientButton";
+import "./style.css";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 
 const VerifyEmail = () => {
@@ -72,59 +75,57 @@ const VerifyEmail = () => {
     router.push("/signup");
   };
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[linear-gradient(to_right,#FF6666,#C964E2)] ">
-      {/* Background Image */}
-      {/* <div
-            className="absolute bg-cover bg-center w-full"
-            style={{
-                backgroundImage: `url('images/squiggles/squiggle_01.png')`,
-                backgroundSize: "cover",
-                width: '50%',  
-                height: '50%', 
-                zIndex: -1,     
-            }}
-            ></div> */}
-      <div className="bg-white rounded-lg shadow-lg py-8 px-8 sm:py-6 sm:px-8 max-w-[90%] md:max-w-[600px] w-full shadow-black">
-        <h1 className="text-[28px] font-semibold text-[#2F2B3D]/[90%] mb-6 text-center mt-4">
-          {loading ? "Verificando seu e-mail. Aguarde, por favor..." : message}
-        </h1>
-        {!loading && (
-          <div className="mt-4">
-            {emailValidated && (
-              <div>
-                <p className="text-[18px] text-[#2F2B3D]/[70%] mb-1 text-center">
-                  Seu cadastro foi validado com sucesso!
-                </p>
-                <p className="text-[18px] text-[#2F2B3D]/[70%] mb-8 text-center">
-                  Faça login e acesse nossa plataforma.
-                </p>
-                <Button onClick={handleLoginRedirect} className="mb-4 p-2">
-                  Login
-                </Button>
-              </div>
-            )}
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-[rgb(255,255,255,0.95)]">
+            <div className="relative w-full min-h-screen h-full flex flex-col items-center justify-center px-[10vw] sd:px-[15vw] md:px-[20vw] overflow-hidden">
+                {/* Background with rotation animation */}
+                <div
+                    className={`absolute flex justify-center items-center 
+                        h-full w-[100vh] bg-cover bg-no-repeat z-[-10]
+                        ${loading ? "animate-spin-slow" : ""}`}
+                    style={{
+                        backgroundImage: "url('/images/identidade-visual/Ativo-1@5.png')",
+                        transform: 'rotate(0deg)',
+                    }}
+                ></div>
 
-            {!emailValidated && (
-              <div>
-                <p className="text-[18px] text-[#2F2B3D]/[70%] mb-1 text-justify">
-                  Pedimos desculpa, infelizemnte ocorreu algum erro e seu
-                  cadastro não foi validado.
-                </p>
-                <p className="text-[18px] text-[#2F2B3D]/[70%] mb-8 text-justify">
-                  Clique no botão para tentarmos enviar outro e-mail de
-                  verificação.
-                </p>
-                <Button onClick={handleSignUpRedirect} className="mb-4 p-2">
-                  Enviar e-mail
-                </Button>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                <div className="bg-white rounded-lg shadow-lg py-8 px-8 sm:py-6 sm:px-8 max-w-[90%] md:max-w-[600px] w-full shadow-black">
+                    <h1 className="text-[28px] font-semibold text-[#2F2B3D]/[90%] mb-6 text-center mt-4">
+                        {loading ? "Verificando seu e-mail. Aguarde, por favor..." : message}
+                    </h1>
+                    {!loading && (
+                        <div className="mt-4">
+                            {emailValidated ? (
+                                <div>
+                                    <p className="text-[18px] text-[#2F2B3D]/[70%] mb-1 text-center">
+                                        Seu cadastro foi validado com sucesso!
+                                    </p>
+                                    <p className="text-[18px] text-[#2F2B3D]/[70%] mb-8 text-center">
+                                        Faça login e acesse nossa plataforma.
+                                    </p>
+                                    <Button onClick={handleLoginRedirect} className="mb-4 p-2">
+                                        Login
+                                    </Button>
+                                </div>
+                            ) : (
+                                <div>
+                                    <p className="text-[18px] text-[#2F2B3D]/[70%] mb-1 text-justify">
+                                        Pedimos desculpa, infelizmente ocorreu algum erro e seu cadastro não foi validado.
+                                    </p>
+                                    <p className="text-[18px] text-[#2F2B3D]/[70%] mb-8 text-justify">
+                                        Clique no botão para tentarmos enviar outro e-mail de verificação.
+                                    </p>
+                                    <Button onClick={handleSignUpRedirect} className="mb-4 p-2">
+                                        Enviar e-mail
+                                    </Button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default VerifyEmail;
