e12473415271e0a57dd2eef040e222ac077acb36
Bruno Freitas <brunoatf257@gmail.com>
2024-10-29 19:51:35 -0300

    Revert "Changing middleware"
    This reverts commit c1b8557c1995d61a1d6395c5a2b9f4dea3b7770a.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from app.routes.auth import router as auth_router
 from app.routes.mentoring import router as mentoring_router
 #from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware as CORSMiddleware 
 from fastapi.responses import JSONResponse
 
@@ -15,30 +14,18 @@ front_end_url = os.getenv("FRONT_END_BASE_URL", None)
 if front_end_url is None:
     raise ValueError("FRONT_END_BASE_URL is not set")
 
-#app = FastAPI()
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "https://centro-de-carreiras-patronos-722033123279.us-central1.run.app",           
-#         "http://localhost:3000", 
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],        
-#     allow_headers=["*"],       
-# )
+app = FastAPI()
 
-middleware = [
-    Middleware(
-        CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
-]
-
-app = FastAPI(middleware=middleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://centro-de-carreiras-patronos-722033123279.us-central1.run.app",           
+        "http://localhost:3000", 
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],        
+    allow_headers=["*"],       
+)
 
 app.include_router(mentoring_router)
 app.include_router(auth_router)
