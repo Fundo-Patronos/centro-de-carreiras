ca17ace8f84ef1059473700cafb23c0b59089372
Bruno Freitas <brunoatf257@gmail.com>
2024-10-29 22:12:05 -0300

    fix front end url env var for cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-722033123279.us-central1.run.app", "http://localhost:3000"],
+    allow_origins=[front_end_url, "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
