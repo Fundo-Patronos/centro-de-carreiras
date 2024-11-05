567571849586c03e5b26f7a733ec64257c36401b
Andreas Cisi Ramos <82724293+TheDeas343@users.noreply.github.com>
2024-11-05 12:55:33 -0300

    CORS error - Testing allow_origins

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[front_end_url, "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
