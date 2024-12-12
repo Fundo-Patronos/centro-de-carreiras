488bb2e74b1d1041383144174cff78a7485db233
Andreas Cisi Ramos <deascisi@hotmail.com>
2024-12-12 18:17:21 -0300

    CORS - Testing url fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         front_end_url,
-        "https://carreiras.patronos.org/",
+        "https://carreiras.patronos.org",
         "https://frontend-722033123279.us-central1.run.app",
         "http://localhost:3000",
     ],
