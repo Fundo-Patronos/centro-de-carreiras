b5d1e442493fc7c3dcc1f13dadb9751b4ad77755
Bruno Freitas <brunoatf257@gmail.com>
2024-12-18 00:46:48 -0300

    added description field to opportunity and staging env

--- frontend/src/app/vagas/page.tsx
+++ frontend/src/app/vagas/page.tsx
@@ -6,7 +6,11 @@ import Hero from "@/components/Vagas/Hero";
 export default function Mentoria() {
   const [isLoading, setIsLoading] = useState(true); // Track loading state
 
-  // This function will be triggered when the iframe is fully loaded
+  const iframeUrl =
+    process.env.NEXT_PUBLIC_STAGING === "true"
+      ? "https://airtable.com/embed/app4uSEqO2S03EO5X/shr7wJKJ7gzgd0K72?"
+      : "https://airtable.com/embed/app4uSEqO2S03EO5X/shrNy5EvFgy1ZooZj?";
+
   const handleIframeLoad = () => {
     setIsLoading(false);
   };
@@ -15,21 +19,19 @@ export default function Mentoria() {
     <Layout currentPage="vagas">
       <Hero /> {/* Hero Section */}
       {/* Main Content */}
-      <div className=" flex flex-col items-center justify-center bg-white">
+      <div className="flex flex-col items-center justify-center bg-white">
         {/* Display loading spinner while iframe is loading */}
         {isLoading && (
           <div className="flex items-center justify-center mt-8">
             <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-red-600"></div>
           </div>
         )}
-
-        {/* Iframe with onLoad event to stop the loading spinner */}
         <iframe
-          src="https://airtable.com/embed/app4uSEqO2S03EO5X/shr7wJKJ7gzgd0K72?viewControls=on"
+          src={iframeUrl}
           width="100%"
-          height="533"
-          onLoad={handleIframeLoad} // Trigger when iframe finishes loading
-          className={isLoading ? "hidden" : ""} // Hide iframe until it's loaded
+          height="650"
+          onLoad={handleIframeLoad}
+          className={isLoading ? "hidden" : ""}
         ></iframe>
       </div>
     </Layout>
