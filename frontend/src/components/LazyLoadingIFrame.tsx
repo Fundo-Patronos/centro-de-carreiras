7b1cade10f5ee27c2a636ccfcd0c8e5a60bf2ce7
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2025-03-31 19:57:24 -0300

    feat: create lazy loading IFrame component

---
+++ frontend/src/components/LazyLoadingIFrame.tsx
@@ -0,0 +1,38 @@
+interface LazyLoadingIframeProps {
+  loadingMessage: string;
+  isFrameVisible: boolean;
+  iframeUrl: string;
+  isLoading: boolean;
+  setIsLoading: (_: boolean) => void;
+}
+
+export default function LazyLoadingIframe(props: LazyLoadingIframeProps) {
+  return (
+    <>
+      {props.isLoading && (
+        <div className="flex flex-col items-center justify-center mt-8 mb-8 gap-4">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-red-600"></div>
+          <p className="text-gray-600 text-lg">{props.loadingMessage}</p>
+        </div>
+      )}
+
+      <iframe
+        src={props.iframeUrl}
+        loading="lazy"
+        style={
+          props.isFrameVisible
+            ? {}
+            : {
+                visibility: "hidden",
+              }
+        }
+        width="100%"
+        height={props.isFrameVisible ? "650" : "0"}
+        onLoad={() => {
+          props.setIsLoading(false);
+        }}
+        className="shadow-lg rounded-lg"
+      ></iframe>
+    </>
+  );
+}
